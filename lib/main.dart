d43157403ac090da970fbddd3ac62571876ef2e2
yanlele <331393627@qq.com>
2022-05-22 21:48:39 +0800

    feat: 更改样式

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 main() {
   // 1. 调用 runApp 函数, 启动一个程序
-  runApp(Text(
+  const text = Text(
     "Hello World",
     textDirection: TextDirection.ltr,
-  ));
+    style: TextStyle(
+        fontSize: 60,
+        color: Colors.orange
+    ),
+  );
+  runApp(text);
 }

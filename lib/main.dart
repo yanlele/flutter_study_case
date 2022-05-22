ef763490e88683e14bd680c442db7e6d839a779b
yanlele <331393627@qq.com>
2022-05-22 22:57:40 +0800

    feat: 抽离

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
+main() => runApp(const MyApp());
+
+/// Widget:
+///  有状态的 Widget: StateFulWidget 在运行时有一些状态是需要更改的
+///  无状态的 Widget: StatelessWidget 在运行时，无状态更改
+///  最终形成的一个 widget 数
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // 重写 build 方法
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Scaffold 脚手架， 可以快速搭建页面
+      home: YLHomeBody(),
+    );
+  }
+}
+
+class YLHomeBody extends StatelessWidget {
+  // 必须要构造这个， 否则应用的时候会抛错
+  const YLHomeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: Header(),
+      body: YLContentBody(),
+    );
+  }
+}
+
 class Header extends StatelessWidget implements PreferredSizeWidget {
   const Header({Key? key}) : super(key: key);
 
@@ -18,35 +51,16 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => const Size.fromHeight(60);
 }
 
-main() {
-  // 1. 调用 runApp 函数, 启动一个程序
-  const text = Text(
-    "Hello World",
-    // textDirection: TextDirection.ltr,
-    style: TextStyle(fontSize: 30, color: Colors.orange),
-  );
-  const center = Center(child: text);
+class YLContentBody extends StatelessWidget {
+  const YLContentBody({Key? key}) : super(key: key);
 
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    // Scaffold 脚手架， 可以快速搭建页面
-    home: Scaffold(
-      // appBar: AppBar(), // 这个会跑错， 非常无语........
-      appBar: Header(),
-      body: center,
-    ),
-  ));
-}
-
-/// Widget:
-///  有状态的 Widget: StateFulWidget 在运行时有一些状态是需要更改的
-///  无状态的 Widget: StatelessWidget 在运行时，无状态更改
-class YLHomePage extends StatelessWidget {
-  const YLHomePage({Key? key}) : super(key: key);
-
-  // 重写 build 方法
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello World");
+    return const Center(
+      child: Text(
+        "Hello World",
+        style: TextStyle(fontSize: 30, color: Colors.orange),
+      ),
+    );
   }
 }

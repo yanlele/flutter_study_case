9ecc40e2cad2fb60bba06a0b9509920be314acb3
yanlele <331393627@qq.com>
2022-05-25 08:37:01 +0800

    feat: context

---
+++ lib/flutter_application_demo/02_widget_yl_context.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/*
+context 使用
+* */
+class YLContextRoute extends StatelessWidget {
+  const YLContextRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Context 测试")),
+      body: Builder(builder: (context) {
+        // 在 widget 树中向上查找最近的父级`Scaffold`  widget
+        Scaffold scaffold = context.findAncestorWidgetOfExactType() as Scaffold;
+        // 直接返回 AppBar的title， 此处实际上是Text("Context测试")
+        return (scaffold.appBar as AppBar).title as Widget;
+      }),
+    );
+  }
+}

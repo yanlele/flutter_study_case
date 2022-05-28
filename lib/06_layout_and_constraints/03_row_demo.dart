e5e1f2c5421384d26f08449cd8100c6da0406f38
yanlele <331393627@qq.com>
2022-05-28 22:40:08 +0800

    feat: Row Column 组件

---
+++ lib/06_layout_and_constraints/03_row_demo.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class YLRowDemo1 extends StatelessWidget {
+  const YLRowDemo1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(" hello world "),
+            Text(" I am Jack "),
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(" hello world "),
+            Text(" I am Jack "),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          textDirection: TextDirection.rtl,
+          children: const <Widget>[
+            Text(" hello world "),
+            Text(" I am Jack "),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          verticalDirection: VerticalDirection.up,
+          children: const <Widget>[
+            Text(" hello world ", style: TextStyle(fontSize: 30.0),),
+            Text(" I am Jack "),
+          ],
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '06_layout_and_constraints/02_unconstrained_box.dart';
+import '06_layout_and_constraints/03_row_demo.dart';
 
 main() => runApp(const MyApp());
 
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       // Scaffold 脚手架， 可以快速搭建页面
       home: Scaffold(
         appBar: AppBar(title: const Text("我是title")),
-        body: const YLUnconstrainedBox(),
+        body: const YLRowDemo1(),
       ),
     );
   }

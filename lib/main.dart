83078eae2a2106875b66423b71f9bfaf125c999a
yanlele <331393627@qq.com>
2022-06-05 22:30:27 +0800

    feat: 初始化

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+// import 'package:flutterappdemo1/common/global.dart';
 
 import 'examples/handle_application_widget_by_07/demo3_future_builder/index.dart';
 
-main() => runApp(const MyApp());
+void main() => runApp(const MyApp());
+
+// main() => Global.init().then((_) => runApp(const MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // 重写 build 方法
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     return MaterialApp(
       title: "沙雕",
       debugShowCheckedModeBanner: false,

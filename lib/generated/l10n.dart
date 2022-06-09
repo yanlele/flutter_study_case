a5688b97a3c83e4e6193d6b4992f8a11c0754ee4
yanlele <331393627@qq.com>
2022-06-09 08:13:02 +0800

    feat: 添加国际化

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -49,6 +49,176 @@ class GmLocalizations {
   static GmLocalizations? maybeOf(BuildContext context) {
     return Localizations.of<GmLocalizations>(context, GmLocalizations);
   }
+
+  /// `Auto`
+  String get auto {
+    return Intl.message(
+      'Auto',
+      name: 'auto',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `cancel`
+  String get cancel {
+    return Intl.message(
+      'cancel',
+      name: 'cancel',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Github`
+  String get home {
+    return Intl.message(
+      'Github',
+      name: 'home',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Language`
+  String get language {
+    return Intl.message(
+      'Language',
+      name: 'language',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Login`
+  String get login {
+    return Intl.message(
+      'Login',
+      name: 'login',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `logout`
+  String get logout {
+    return Intl.message(
+      'logout',
+      name: 'logout',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Are you sure you want to quit your current account?`
+  String get logoutTip {
+    return Intl.message(
+      'Are you sure you want to quit your current account?',
+      name: 'logoutTip',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `No description yet !`
+  String get noDescription {
+    return Intl.message(
+      'No description yet !',
+      name: 'noDescription',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Password`
+  String get password {
+    return Intl.message(
+      'Password',
+      name: 'password',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Password required!`
+  String get passwordRequired {
+    return Intl.message(
+      'Password required!',
+      name: 'passwordRequired',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Setting`
+  String get setting {
+    return Intl.message(
+      'Setting',
+      name: 'setting',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Theme`
+  String get theme {
+    return Intl.message(
+      'Theme',
+      name: 'theme',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Flutter APP`
+  String get title {
+    return Intl.message(
+      'Flutter APP',
+      name: 'title',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `User Name`
+  String get userName {
+    return Intl.message(
+      'User Name',
+      name: 'userName',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `User name or password is not correct!`
+  String get userNameOrPasswordWrong {
+    return Intl.message(
+      'User name or password is not correct!',
+      name: 'userNameOrPasswordWrong',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `User name required!`
+  String get userNameRequired {
+    return Intl.message(
+      'User name required!',
+      name: 'userNameRequired',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `yes`
+  String get yes {
+    return Intl.message(
+      'yes',
+      name: 'yes',
+      desc: '',
+      args: [],
+    );
+  }
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<GmLocalizations> {
@@ -57,6 +227,8 @@ class AppLocalizationDelegate extends LocalizationsDelegate<GmLocalizations> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'en', countryCode: 'US'),
+      Locale.fromSubtags(languageCode: 'zh', countryCode: 'CN'),
     ];
   }
 

0efb4db241d35b1122424abf172d18d47807d8b4
yanlele <331393627@qq.com>
2022-06-09 08:29:22 +0800

    feat: main home

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
-// import 'package:flutterappdemo1/common/global.dart';
+import 'package:flutterappdemo1/generated/l10n.dart';
+import 'package:flutterappdemo1/routes/home_page/index.dart';
+import 'package:flutterappdemo1/states/profile_change_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
-import 'examples/handle_application_widget_by_07/demo3_future_builder/index.dart';
+import 'common/global.dart';
 
-void main() => runApp(const MyApp());
-
-// main() => Global.init().then((_) => runApp(const MyApp()));
+main() => Global.init().then((_) => runApp(const MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  List<SingleChildWidget> get providers => [
+        ChangeNotifierProvider.value(value: ThemeModel()),
+        ChangeNotifierProvider.value(value: UserModel()),
+        ChangeNotifierProvider.value(value: LocalModel()),
+      ];
+
+  GenerateAppTitle generateAppTitle(BuildContext ctx) => (ctx) => GmLocalizations.of(ctx).title;
+
   // 重写 build 方法
   @override
   Widget build(context) {
-    return MaterialApp(
-      title: "沙雕",
-      debugShowCheckedModeBanner: false,
-      // Scaffold 脚手架， 可以快速搭建页面
-      home: Scaffold(
-        appBar: AppBar(title: const Text("我是title")),
-        body: const FutureBuilderDemo(),
+    return MultiProvider(
+      providers: providers,
+      child: Consumer2<ThemeModel, LocalModel>(
+        builder: (context, themeModel, localModel, child) {
+          return MaterialApp(
+            // primaryColor是primarySwatch的一种[500]，设置primarySwatch会更好，覆盖的范围更大
+            theme: ThemeData(primarySwatch: themeModel.theme),
+            // 跟title差不多，但是onGenerateTitle可以做本地化
+            onGenerateTitle: generateAppTitle(context),
+            // 首页
+            home: const HomePage(),
+            // 设置语言
+            locale: localModel.getLocale(),
+            // 设置支持的语言，使用Intl插件工具后，直接使用就好
+            supportedLocales: GmLocalizations.delegate.supportedLocales,
+
+          );
+        },
       ),
     );
   }
